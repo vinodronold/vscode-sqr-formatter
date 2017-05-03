d7f673903878729b12442f6d6ad3c77c818e6597
Vinod Ronold <vinodronoldbe@gmail.com>
2017-05-03 10:41:55 -0400

    Cleaned Activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,12 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window } from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export const activate = (context: ExtensionContext) => {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    //console.log('Congratulations, your extension "vscode-sqr-formatter" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.formatSQR', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('SQR Formatted Successfully.!!');
-    });
-
-    context.subscriptions.push(disposable);
+    let formatDisposable = commands.registerCommand('extension.formatSQR', () => {
+        window.showInformationMessage('SQR formatted successfully.!!')
+    })
+    context.subscriptions.push(formatDisposable)
 }
 
 // this method is called when your extension is deactivated
