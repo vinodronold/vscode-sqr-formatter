fe2155028b328add83d21af2ec0b97aec6317b46
Vinod Ronold <vinodronoldbe@gmail.com>
2017-05-03 15:10:43 -0400

    Implemented Indend Logic

--- src/format.ts
+++ src/format.ts
@@ -1,12 +1,51 @@
 'use strict'
 import { EOL } from 'os'
 
-const format = (doc: string) =>{
+const reBegin = new RegExp(/^begin|declare/, "i")
+const reEnd = new RegExp(/^end/, "i")
+const reComments = new RegExp(/^!/)
+let indentLevel: number = 0
+
+const decrementIndend = () => {
+    indentLevel--
+    if (indentLevel < 0) {
+        indentLevel = 0
+    }
+}
+
+const formattedLine = (ln: string) => {
+    console.log(indentLevel, ln)
+    return (' '.repeat(indentLevel * 4) + ln)
+}
+
+const formatLine = (line: string) => {
+    switch (true) {
+        case reComments.test(line):
+            // DO NOT INDEND 
+            //console.log('comment')
+            break;
+        case reEnd.test(line):
+            decrementIndend()
+            line = EOL + formattedLine(line)
+            //console.log('end')
+            break
+        case reBegin.test(line):
+            line = formattedLine(line) + EOL
+            indentLevel++
+            //console.log('begin')
+            break
+        default:
+            line = formattedLine(line)
+        //console.log('default')
+    }
+    return line
+}
+
+const format = (doc: string) => {
     console.log('Begin Format . . . ')
     let docLines: Array<string> = doc.split(EOL)
-    console.log(docLines);
-    
-    return docLines.map(line => line.trim() + ' Formated').join(EOL)
+    docLines = docLines.map(l => l.trim()).filter((l) => (l.length > 0))
+    return docLines.map(formatLine).join(EOL)
 }
 
 export default format
