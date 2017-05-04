85271bbe7760857ee85734046f7ad734082598ec
Vinod Ronold <vinodronoldbe@gmail.com>
2017-05-04 14:16:00 -0400

    Improved Begin-Select Formatting

--- src/format.ts
+++ src/format.ts
@@ -1,42 +1,71 @@
 'use strict'
 import { EOL } from 'os'
 
-const reBegin = new RegExp(/^(begin|declare|if|#if|evaluate|when)/, "i")
+const reBegin = new RegExp(/^(begin|declare)/, "i")
+const reConditions = new RegExp(/^(if|#if|evaluate|when)/, "i")
 const reEnd = new RegExp(/^(end|#end|else|#else|break)/, "i")
 const reComments = new RegExp(/^!/)
-let indentLevel: number = 0
+let indendLevel: number = 0
 
-const incrementIndend = () => indentLevel++
+const reBeginSelect = new RegExp(/^(begin-select)/, "i")
+const reEndSelect = new RegExp(/^(end-select)/, "i")
+let beginSelectBlock: boolean = false
+
+let formattedDoc: Array<string> = []
+
+const incrementIndend = () => indendLevel++
 const decrementIndend = () => {
-    indentLevel--
-    if (indentLevel < 0) {
-        indentLevel = 0
+    indendLevel--
+    if (indendLevel < 0) {
+        indendLevel = 0
     }
 }
 
-const formattedLine = (ln: string) => {
-    console.log(indentLevel, ln)
-    return (' '.repeat(indentLevel * 4) + ln)
+const formattedLine = (ln: string, level: number) => {
+    //console.log(level, ln)
+    return (' '.repeat(level * 4) + ln)
 }
 
-const formatLine = (line: string) => {
+const formatDoc = (line: string) => {
+
+    if (beginSelectBlock) {
+        if (line.substring(0, 1) === ' ') {
+            beginSelectBlock = false
+        } else {
+            return line.trim()
+        }
+    }
+
+    line = line.trim()
     switch (true) {
         case reComments.test(line):
             // DO NOT INDEND 
             //console.log('comment')
             break
+        case reBeginSelect.test(line):
+            line = EOL + line
+            beginSelectBlock = true
+            //console.log('BeginSelect')
+            break
+        case reEndSelect.test(line):
+            line = line + EOL
+            //console.log('EndnSelect')
+            break
         case reBegin.test(line):
-            line = EOL + formattedLine(line) + EOL
+            line = EOL + formattedLine(line, indendLevel)
             incrementIndend()
             //console.log('begin')
             break
         case reEnd.test(line):
             decrementIndend()
-            line = EOL + formattedLine(line) + EOL
+            line = formattedLine(line, indendLevel) + EOL
             //console.log('end')
             break
+        case reConditions.test(line):
+            line = EOL + formattedLine(line, indendLevel)
+            incrementIndend()
         default:
-            line = formattedLine(line)
+            line = formattedLine(line, indendLevel)
         //console.log('default')
     }
     return line
@@ -45,8 +74,8 @@ const formatLine = (line: string) => {
 const format = (doc: string) => {
     console.log('Begin Format . . . ')
     let docLines: Array<string> = doc.split(EOL)
-    docLines = docLines.map(l => l.trim()).filter((l) => (l.length > 0))
-    return docLines.map(formatLine).join(EOL)
+    docLines = docLines.filter((l) => (l.trim().length > 0))
+    return docLines.map(formatDoc).join(EOL)
 }
 
 export default format
